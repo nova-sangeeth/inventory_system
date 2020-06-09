0743fe70821a76394f38394c1790f921e878efaf
nova-sangeeth <novasangeeth@outlook.com>
2020-06-10 00:45:03 +0530

    made changes to the models, making all th fields NULLABLE FIELDS.

--- inventory_system/models.py
+++ inventory_system/models.py
@@ -6,31 +6,33 @@ from django.db import models
 
 class Electronics(models.Model):
 
-    type = models.CharField(max_length=256, blank=False)
+    type = models.CharField(max_length=256, null=True, blank=False)
     price = models.IntegerField()
     brand = models.CharField(max_length=256, null=True)
     choices = (
-        ('AVAILABLE', 'Items to be PURCHASED'),
-        ('SOLD', 'Item SOLD'),
-        ('RESTOCKING', 'Items to be RESTOCKED')
+        ("AVAILABLE", "Items to be PURCHASED"),
+        ("SOLD", "Item SOLD"),
+        ("RESTOCKING", "Items to be RESTOCKED"),
     )
-    status = models.CharField(max_length=100, choices=choices, default='SOLD')
+    status = models.CharField(
+        max_length=100, choices=choices, default="SOLD", null=True
+    )
     # sku = models.TextField(max_length=64, null=True)
-    issues = models.CharField(max_length=100, default='No issues')
+    issues = models.CharField(max_length=100, null=True, default="No issues")
     shipping_choices = (
-        ('SHIPPABLE', 'item can be shipped'),
-        ('NON-SHIPPABLE', 'item cannot be shipped'),
-        ('SPECIAL-SHIPPING', 'item will be shipped later')
-
+        ("SHIPPABLE", "item can be shipped"),
+        ("NON-SHIPPABLE", "item cannot be shipped"),
+        ("SPECIAL-SHIPPING", "item will be shipped later"),
     )
     shipping = models.CharField(
-        max_length=100, choices=shipping_choices, default='SHIPPABLE')
+        null=True, max_length=100, choices=shipping_choices, default="SHIPPABLE"
+    )
 
     class Meta:
         abstract = True
 
     def __str__(self):
-        return 'Type: {0} Price : {1}'. format(self.type, self.price)
+        return "Type: {0} Price : {1}".format(self.type, self.price)
 
 
 class laptops(Electronics):
