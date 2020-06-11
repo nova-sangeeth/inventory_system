1da098168600918f79ebb5b0b6013e40e2e8e6cf
nova-sangeeth <novasangeeth@outlook.com>
2020-06-11 21:36:30 +0530

    added new model "user profile"

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class user_profile(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256, null=True)
+    age = models.PositiveIntegerField(null=True)
+    address = models.CharField(max_length=256, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    pincode = models.CharField(max_length=7, null=True)
+
+    def __str__(self):
+        return self.user
