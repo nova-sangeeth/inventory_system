9c7c3d57e03915de00e14669532f77fc7b042635
nova-sangeeth <novasangeeth@outlook.com>
2020-06-16 21:08:43 +0530

    added the edit_profile to urls

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import profile, register
+from .views import profile, register, edit_profile
 
 urlpatterns = [
     url(r"^profile/$", profile, name="profile"),
     url(r"^register/$", register, name="register"),
+    url(r"^edit_profile/$", edit_profile, name="edit_profile"),
 ]
